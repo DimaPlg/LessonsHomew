44a0d2eb1efc0538b418ba3b0e5916c5a5722ed3
Dima <pleshko.dima19@gmail.com>
2022-09-25 21:47:52 +0300

    Task 6

---
+++ Lesson2_6_task/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        num = sc.nextInt();
+        String str_num = String.valueOf(num);
+        System.out.println(new StringBuilder(str_num).reverse().toString());
+    }
+}
